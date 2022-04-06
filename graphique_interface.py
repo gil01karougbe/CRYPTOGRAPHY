6ef66c77c7b69463c4e18d22fb7cd84f5b7d774b
gilles karougbe <essognimgilleskarougbe01@gmail.com>
2022-04-06 10:07:07 +0000

    ajout d'image ico et personalisation de l'instance window

--- graphique_interface.py
+++ graphique_interface.py
@@ -1,5 +1,19 @@
 from tkinter import *
+#création de l'instance fenètre
 window=Tk()
+#personnalisation de la fenètre
+window.title="Cryptography"
+window.geometry("1080x720")
+window.minsize(480,360)
+window.iconbitmap("image2.ico")
+#window.config(background="gray")
 
+
+
+
+
+
+
+
+#affichage
 window.mainloop()
-
